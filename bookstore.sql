211a3453bda65eaa4b8a1085851af9f7407cddcd
priscilla nzula <danielpriscilla61@gmail.com>
2025-04-11 18:51:14 +0300

    main file

--- bookstore.sql
+++ bookstore.sql
@@ -1,3 +1,21 @@
-CREATE DATABASE bookstoredb;
+USE bookstoredb;
+-- creating new users 
+CREATE USER 'leisha'@'localhost' IDENTIFIED BY '1234';
+CREATE USER 'faith'@'localhost' IDENTIFIED BY '1234';
 
-use bookstoredb;
+GRANT ALL PRIVILEGES ON bookstoredb.* TO 'leisha'@'localhost';
+GRANT ALL PRIVILEGES ON bookstoredb.* TO 'faith'@'localhost';
+
+-- CREATE  TABLE Order_line(
+-- orderLine_id INT NOT NULL,
+-- order_id INT NOT NULL,
+-- book_id INT NOT NULL,
+-- quantity INT NOT NULL,
+-- unitPrice FLOAT NOT NULL,
+
+-- PRIMARY KEY(orderLine_id),
+-- FOREIGN KEY(order_id) REFERENCES Cust_order(order_id),
+-- FOREIGN KEY(book_id) REFERENCES Book(book_id)
+-- );
+
+
