aa40512312f04a26a7d1631f0ade51ebd10f78a4
priscilla nzula <danielpriscilla61@gmail.com>
2025-04-14 18:36:34 +0300

    update bookstoredb

--- bookstore.sql
+++ bookstore.sql
@@ -152,7 +152,8 @@ statusName VARCHAR(100) NOT NULL,
 PRIMARY KEY(orderStatus_id)
 );
 
-
+-- Access control
+-- Create roles for different user groups
 CREATE ROLE admin_role,managerial_role,sales_role, inventory_role, customer_role; 
 
 -- create users
